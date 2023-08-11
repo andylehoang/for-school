f6be8e61c5a863294a9a025bdf940034900cf755
andylehoang <42783594+andylehoang@users.noreply.github.com>
2023-08-11 19:53:50 +0000

    imma use codespace

--- project/testing/main.java
+++
@@ -1,5 +0,0 @@
-class GUI {
-  public static void main(String[] args[]){
-    System.out.println("Hello World");
-  }
-}
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.arange(15, dtype=np.float16)
+print(x)
